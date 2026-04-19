fec8b6daf2e009b9a0cfa3fb7dbb51b71bdade7d
Amit Pandey <amitpandeymtr2002@gmail.com>
2026-04-20 03:06:36 +0530

    Add files via upload

---
+++ C# Day4/CollectionList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+        list.Add("Iphone");
+        list.Add("Oneplus");
+        list.Add("Redmi");
+        list.Add("Google");
+        foreach (String i in list)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
